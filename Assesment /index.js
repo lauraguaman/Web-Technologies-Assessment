b213ec55498145d47b18fa433c81b5de6404476c
lauraguaman <0guaml23@solent.ac.uk>
2025-04-30 18:48:06 +0100

    #assessment changes

--- Assesment /index.js
+++ Assesment /index.js
@@ -1,30 +1,3 @@
-// Código para abrir y cerrar el submenú del navbar
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menuList = document.querySelector('.menu-list');
-  
-    if (menuToggle && menuList) {
-      menuToggle.addEventListener('click', () => {
-        if (menuList.style.display === 'flex') {
-          menuList.style.display = 'none';
-        } else {
-          menuList.style.display = 'flex';
-        }
-      });
-    }
-  });
-
-//page 7 information//
-
-// Función para redirigir al inicio
-function goHome() {
-  window.location.href = "index.html";
-}
-
-// Función para redirigir a otras páginas
-function goToPage(page) {
-  window.location.href = page;
-}
 
 
 
